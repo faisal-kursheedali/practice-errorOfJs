7eb1c0b62753335f2b0f43edd58e429d4a92a2d8
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-06-03 08:29:11 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// Refrential Error
+// console.log(a);
+// console.log(a);
+// let a;
+// Refrential Error
+
+// Syntax Error
+//It is the rule to not to re-declare the variable uning let key word
+// let a=10;
+// let a=100;
+// const a;
+// Syntax Error
+
+// Type Error
+// it give error becoz variable b is of type const which can not be reassign
+// const b=200;
+// b=300;
+// Type Error
